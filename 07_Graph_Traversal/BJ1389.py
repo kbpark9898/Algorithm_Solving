e0f697e5d15f6e680d5f23681d60476c67035a3b
kbpark9898 <kbpark9898@gmail.com>
2021-04-02 17:50:23 +0900

    0402_2sol_BJ1389 modify

--- 07_Graph_Traversal/BJ1389.py
+++ 07_Graph_Traversal/BJ1389.py
@@ -5,15 +5,7 @@ for i in range(friendship):
     adj[source][dst]=1
     adj[dst][source]=1
 
-
-kevin_max=10000
-visited=[[kevin_max for i in range(user+1)] for i in range(user+1)]
-for i in range(user+1):
-    for j in range(user+1):
-        if i==j:
-            visited[i][j] =0
-
-def floyd_warshal(adj, visited):
+def floyd_warshal(adj):
     global user
     for k in range(1, user+1):
         for i in range(1, user+1):
@@ -25,20 +17,12 @@ def floyd_warshal(adj, visited):
                         else:
                             adj[i][j] = min(adj[i][j], adj[i][k]+adj[k][j])
                         
-
 result=[0 for i in range(user+1)]
 
-floyd_warshal(adj, visited)
+floyd_warshal(adj)
 
 for i in range(1, user+1):
     result[i] = sum(adj[i])
 
-result[0]=kevin_max
 solution = result.index(min(result[1:-1]))
 print(solution)
-
-
-        
-
-
-
