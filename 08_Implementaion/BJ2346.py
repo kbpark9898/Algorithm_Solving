77ebff7a3c9092bbd6a5d7410d23503fca243c7c
kbpark9898 <kbpark9898@gmail.com>
2021-04-08 21:26:18 +0900

    0408_1sol

---
+++ 08_Implementaion/BJ2346.py
@@ -0,0 +1,20 @@
+k= int(input())
+ballon = list(map(int, input().split()))
+ballon_count = k
+index=0
+data = ballon.pop(index)
+index_match = [i for i in range(1, k+1)]
+solution=[]
+solution.append(index_match.pop(index))
+while ballon:
+    if data>0:
+        index = (index+data-1)%len(ballon)
+        data=ballon.pop(index)
+        solution.append(index_match.pop(index))
+    else:
+        index = (index+data)%len(ballon)
+        data=ballon.pop(index)
+        solution.append(index_match.pop(index))
+
+for i in solution:
+    print(i, end=' ')
