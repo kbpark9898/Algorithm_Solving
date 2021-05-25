a8973a6ae5669bb32c019301455e13fc0e2d7754
kbpark9898 <kbpark9898@gmail.com>
2021-05-25 15:36:03 +0900

    0525_1sol

---
+++ 07_Graph_Traversal/BJ14257.py
@@ -0,0 +1,30 @@
+import sys
+if __name__ == "__main__":
+    sys.setrecursionlimit(100000)
+
+    employees, compliments = map(int, sys.stdin.readline().split())
+
+    buha_jikwon=[[] for i in range(employees+1)]
+
+    buha_input = list(map(int, sys.stdin.readline().split()))
+
+    for i in range(1, len(buha_input)):
+        buha_jikwon[buha_input[i]].append(i+1)
+
+    dp=[0 for i in range(employees+1)]
+    def dfs(start):
+        global employees, buha_jikwon, dp
+        emp_num, com_num = start
+        for emp in buha_jikwon[emp_num]:
+            dp[emp] += com_num
+            dfs([emp, dp[emp]])
+
+
+    for i in range(compliments):
+        e_num, c_num=map(int, sys.stdin.readline().split())
+        dp[e_num] += c_num
+    dfs([1, dp[1]])
+        
+
+    for i in range(1, employees+1):
+        print(dp[i], end=' ')
